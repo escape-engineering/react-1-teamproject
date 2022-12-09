fe0a22f52f458a5361acdc8a6660b1b11ab3efcc
escape-engineering <117638805+escape-engineering@users.noreply.github.com>
2022-12-09 21:13:27 +0900

    컴포넌트 생성완료

---
+++ src/component/comment/Comment.js
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+import ButtonComponent from '../button/Button';
+
+import '../button/style.css';
+
+const Comment = () => {
+  return (
+    <CommentWrap>
+      <p>nickname</p>
+      <p>comment</p>
+      <ButtonWrapWrap>
+        <ButtonWrap>
+          <ButtonComponent value="CommentToggle" />
+          <ButtonComponent value="CommentDelete" />
+        </ButtonWrap>
+      </ButtonWrapWrap>
+    </CommentWrap>
+  );
+};
+
+const CommentWrap = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 10px;
+  border-bottom: 1px solid green;
+  display: block;
+`;
+
+const ButtonWrap = styled.div`
+  display: flex;
+  width: 250px;
+  height: 40px;
+  gap: 5px;
+`;
+
+const ButtonWrapWrap = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+`;
+
+export default Comment;
