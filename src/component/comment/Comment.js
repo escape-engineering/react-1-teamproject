f22291e448450592d7f3b1a9b5777b3dde1c4a77
escape-engineering <117638805+escape-engineering@users.noreply.github.com>
2022-12-10 18:14:11 +0900

    button 작동 수정

--- src/component/comment/Comment.js
+++ src/component/comment/Comment.js
@@ -1,27 +1,85 @@
-import styled from "styled-components";
-import ButtonComponent from "../button/Button";
+import styled from 'styled-components';
+import ButtonComponent from '../button/Button';
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
 
 const Comment = () => {
+  const [commentOpen, setCommentOpen] = useState(true);
+  const comments = useSelector((state) => state.todolist.Comment);
+  console.log(comments);
   return (
-    <CommentWrap>
-      <p>nickname</p>
-      <p>comment</p>
-      <ButtonWrapWrap>
-        <ButtonWrap>
-          <ButtonComponent value="CommentToggle" />
-          <ButtonComponent value="CommentDelete" />
-        </ButtonWrap>
-      </ButtonWrapWrap>
-    </CommentWrap>
+    <div>
+      {comments.map((comment) => {
+        return (
+          <div key={comment.commentId}>
+            <CommentWrap isOpen={commentOpen}>
+              <p>{comment.nickname}</p>
+              <p>{comment.commentdesc}</p>
+              <ButtonWrapWrap>
+                <ButtonWrap>
+                  <ButtonComponent
+                    getState={commentOpen}
+                    setState={setCommentOpen}
+                    value="CommentRetouchOpen"
+                  />
+                  <ButtonComponent value="CommentDelete" coLor="red" />
+                </ButtonWrap>
+              </ButtonWrapWrap>
+            </CommentWrap>
+            <RetouchCommentWrap isOpen={commentOpen}>
+              <p>{comment.nickname}</p>
+              <p>comment</p>
+              <RetouchInput />
+              <RetouchButtonWrapWrap>
+                <RetouchButtonWrap>
+                  <ButtonComponent
+                    getState={commentOpen}
+                    setState={setCommentOpen}
+                    value="CommentRetouch"
+                  />
+                </RetouchButtonWrap>
+              </RetouchButtonWrapWrap>
+            </RetouchCommentWrap>
+          </div>
+        );
+      })}
+    </div>
   );
 };
+
+const RetouchCommentWrap = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 10px;
+  border-bottom: 1px solid green;
+  display: ${({ isOpen }) => (isOpen ? 'none' : 'block')};
+`;
+
+const RetouchInput = styled.input`
+  width: 100%;
+  height: 100px;
+  margin-bottom: 10px;
+`;
+
+const RetouchButtonWrapWrap = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const RetouchButtonWrap = styled.div`
+  display: flex;
+  width: 125px;
+  height: 40px;
+  gap: 5px;
+`;
 
 const CommentWrap = styled.div`
   width: 100%;
   height: 100%;
   padding: 10px;
   border-bottom: 1px solid green;
-  display: block;
+  display: ${({ isOpen }) => (isOpen ? 'block' : 'none')};
 `;
 
 const ButtonWrap = styled.div`
