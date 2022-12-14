d5daa152b54c8e5badfc13456aac448d4f6a4d58
escape-engineering <117638805+escape-engineering@users.noreply.github.com>
2022-12-14 15:22:38 +0900

    댓글 추가하기 완료

--- src/redux/modules/todolist.js
+++ src/redux/modules/todolist.js
@@ -70,7 +70,21 @@ export const __getComments = createAsyncThunk(
   }
 );
 // 댓글 추가 하기
-
+export const __postComments = createAsyncThunk(
+  'postComments',
+  async (payload, thunkAPI) => {
+    try {
+      const { data } = await axios.post(
+        'http://localhost:3001/comments',
+        payload
+      );
+      return thunkAPI.fulfillWithValue(data);
+    } catch (err) {
+      console.log(err);
+      return thunkAPI.rejectWithValue(err);
+    }
+  }
+);
 // 댓글 삭제 하기
 export const __deleteComment = createAsyncThunk(
   'deleteComment',
@@ -179,6 +193,22 @@ const todoSlice = createSlice({
       })
       //로딩 완료. 실패 시
       .addCase(__getComments.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      })
+      // -------------------------------------------------------------
+      // 댓글 추가 하기
+      // 로딩 시작
+      .addCase(__postComments.pending, (state) => {
+        state.isLoading = true;
+      })
+      //로딩 완료. 성공 시
+      .addCase(__postComments.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.comments = [action.payload, ...state.comments];
+      })
+      //로딩 완료. 실패 시
+      .addCase(__postComments.rejected, (state, action) => {
         state.isLoading = false;
         state.error = action.payload;
       })
