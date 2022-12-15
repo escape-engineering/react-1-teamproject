303a998d210266910b06ff0c20d87bf612dbdc52
이재정 <ljj9535@gmail.com>
2022-12-15 17:53:11 +0900

    dectool 로컬에서만 활성화

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,9 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from "@reduxjs/toolkit";
 
-import todolist from '../modules/todolist';
+import todolist from "../modules/todolist";
 
 const store = configureStore({
   reducer: { todolist: todolist },
+  devTools: process.env.REACT_APP_URL !== "http://localhost:3000",
 });
 
 export default store;
